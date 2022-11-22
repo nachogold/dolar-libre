1e3e58cdf5ed96865c1d35d3d1df12d00036e4e3
Juan Ignacio Goldberg <jignacio1596@gmail.com>
2022-11-21 21:18:34 -0300

    Added data refresh schedule. Added html auto refresh.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from views import views
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 app.register_blueprint(views, url_prefix="/")
 
--- views.py
+++ views.py
@@ -1,12 +1,27 @@
-#TO DO:
-#AUTOMATIZAR PROCESO DE GET DATA : https://stackoverflow.com/questions/21214270/how-to-schedule-a-function-to-run-every-hour-on-flask
 from flask import Blueprint, render_template
 from modules.get_data import get_data
+from apscheduler.schedulers.background import BackgroundScheduler
+import pandas as pd
+import atexit
 
 views = Blueprint(__name__,"views")
 
-df = get_data()
+#definimos funcion para scrapear data y guardarla en un csv. De acá vamos a tomar la data para mostrar en el html
+def update_data():
+    print('updating data')
+    data = get_data()
+    data.to_csv('dolar-libre/data.csv',index=False)
+
+#corremos por primera vez la funcion
+update_data()
+
+#scheduleamos la funcion para tener data nueva cada 10 minutos
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=update_data, trigger="interval", minutes=10)
+scheduler.start()
 
 @views.route("/")
 def home():
-    return render_template("base.html",df=df)
+    return render_template("index.html",df=pd.read_csv('dolar-libre/data.csv'))
+
+atexit.register(lambda: scheduler.shutdown())
